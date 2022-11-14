e26bbd6998c39e4d72362400664d57ad7b0a74ae
samat_sultanov <samatsayak@gmail.com>
2022-11-14 21:56:22 +0600

    создал модель Product, зарегистрировал ее в админ-панели; добавил данные и создал админ-юзера и фикстуру; создал и накатил миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "balance"]
+    list_display_links = ["name"]
+    filter = ["category"]
+    search_fields = ["name", "description"]
+    exclude = []
+    readonly_fields = ["created_at", "updated_at"]
+
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [('other', 'разное'), ('food', 'еда'), ('drinks', 'напитки')]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Наименование товара")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Описание товара")
+    category = models.CharField(max_length=30, null=False, blank=False, choices=CATEGORY_CHOICES,
+                                default=CATEGORY_CHOICES[0][0])
+    balance = models.PositiveIntegerField(verbose_name="Остаток")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время редактирования")
+
+    def __str__(self):
+        return f"Товар {self.id}: {self.name}"
